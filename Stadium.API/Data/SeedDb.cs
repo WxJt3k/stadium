6f50619aabebaa5f8900e8b63c8fac282ab2809b
lgiraldocorrales <84143921+lgiraldocorrales@users.noreply.github.com>
2023-04-29 10:53:08 -0500

    Create Seed Db

---
+++ Stadium.API/Data/SeedDb.cs
@@ -0,0 +1,41 @@
+﻿using Stadium.Shared.Entities;
+using Stadium.API.Data;
+
+namespace Stadium.API.Data
+{
+    public class SeedDb
+    {
+        private readonly DataContext _context;
+
+        public SeedDb(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task SeedAsync()
+        {
+            await _context.Database.EnsureCreatedAsync();
+            await CheckTicketsAsync();
+        }
+
+        private async Task CheckTicketsAsync()
+        {
+            if (!_context.Tickets.Any())
+            {
+                for (int i = 0; i < 50000; i++)
+                {
+                    var ticket = new Ticket
+                    {
+                        UseDate = new DateTime(2000, 1, 1),
+                        FueUsada = false,
+                        Porteria = "",
+                    };
+
+                    _context.Tickets.Add(ticket);
+                }
+            }
+
+            await _context.SaveChangesAsync();
+        }
+    }
+}
